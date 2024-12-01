3b01f61c6ec5f951a65c3fa3e3f16d0ef79b9654
SudhaJarathi <sudha.sai@gmail.com>
2024-12-01 22:19:57 +0000

    Added more http methods and service.

--- dashboard_api/Controllers/DashboardController.cs
+++ dashboard_api/Controllers/DashboardController.cs
@@ -1,10 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using dashboard_api.Model;
 using dashboard_api.Data;
+using dashboard_api.Services;
 
 
 namespace Dashboard_API.Controllers
@@ -15,11 +12,13 @@ namespace Dashboard_API.Controllers
     {       
         private readonly ILogger<DashboardController> _logger;
         private readonly DashboardDbContext _context;
+        private readonly IDashboardService _dashboardService;
 
-        public DashboardController(ILogger<DashboardController> logger, DashboardDbContext context)
+        public DashboardController(ILogger<DashboardController> logger, DashboardDbContext context, IDashboardService dashboardService)
         {
             _logger = logger;
             _context = context;
+            _dashboardService = dashboardService;
         }
                
 
@@ -32,6 +31,21 @@ namespace Dashboard_API.Controllers
                 Name = "sample"
             };
             return Task.FromResult<ActionResult<IEnumerable<Dashboard>>>(Ok(dashboard));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Dashboard>> CreateDashboard(Dashboard dashboard)
+        {
+            var createdOrderDto = await _dashboardService.CreateDashboardAsync(dashboard);
+
+            return Created($"/api/dashboard/{dashboard.Id}", 1);           
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Dashboard>> UpdateDashboard(int id, Dashboard dashboard)
+        {
+           var updatedDashboard = await _dashboardService.UpdateDashboardAsync(dashboard);
+           return Ok(updatedDashboard);            
         }
     }
 }
---
+++ dashboard_api/Services/IDashboardService.cs
@@ -0,0 +1,11 @@
+﻿using dashboard_api.Model;
+
+namespace dashboard_api.Services
+{
+    public interface IDashboardService
+    {
+        Task<Dashboard> CreateDashboardAsync(Dashboard dashboard);
+        Task<Dashboard> UpdateDashboardAsync(Dashboard dashboard);
+        Task<IEnumerable<Dashboard>> GetDashboardAsync();
+    }
+}
