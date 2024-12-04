2445149302ef4a2297ac0639773b9a04c2115b46
SudhaJarathi <sudha.sai@gmail.com>
2024-12-04 15:55:40 +0000

    Added Log folder installed Auto Mapper and Fluent Validation Added mapper file.

---
+++ dashboard_api/Mapping/ProductProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using dashboard_api.DTOs;
+using dashboard_api.Entities;
+
+namespace dashboard_api.Mapping
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile() 
+        {
+            CreateMap<Product, ProductReadDTO>().ReverseMap();
+            CreateMap<Product, ProductCreateUpdateDTO>().ReverseMap();
+        }
+    }
+}
+
---
+++ dashboard_api/Validators/ProductReadDTOValidator.cs
@@ -0,0 +1,22 @@
+﻿using dashboard_api.DTOs;
+using FluentValidation;
+
+namespace dashboard_api.Validators
+{
+    public class ProductDTOValidator: AbstractValidator<ProductReadDTO>
+    {
+        public ProductDTOValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Product ID is required");
+
+            RuleFor(x => x.Category)
+                .NotEmpty()
+                .WithMessage("Category is required");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than zero.");
+        }
+    }
+}
