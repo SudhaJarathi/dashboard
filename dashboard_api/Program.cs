8e8a10dc0fbe1f383d4d6f4a56c42928f3271b31
SudhaJarathi <sudha.sai@gmail.com>
2024-12-05 16:39:21 +0000

    Refactored and installed serilog added code to create logs using serilog.

--- dashboard_api/Program.cs
+++ dashboard_api/Program.cs
@@ -2,11 +2,42 @@ using dashboard_api.Data;
 using dashboard_api.Mapping;
 using dashboard_api.Repositories;
 using dashboard_api.Services;
+using dashboard_api.Validators;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddAutoMapper(typeof(ProductProfile));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+// Entity Framework Core configurations
+builder.Services.AddDbContext<DashboardDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Add Fluent validator
+builder.Services.AddFluentValidation(
+    options => options.RegisterValidatorsFromAssemblyContaining<ProductDTOValidator>());
+
+//# Serilog configurations with Console log, File log and AppInsight log
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()    
+    .WriteTo.Console()
+    .WriteTo.File("Logs/dashboard.txt", rollingInterval: RollingInterval.Day) // Dev
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,12 +55,6 @@ builder.Services.AddCors(options =>
 // Entity Framework Core configurations
 builder.Services.AddDbContext<DashboardDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddAutoMapper(typeof(ProductProfile));
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
