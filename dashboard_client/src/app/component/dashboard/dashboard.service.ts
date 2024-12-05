8558523e452c36a00ac58a2965d8f3fc29c7a459
SudhaJarathi <sudha.sai@gmail.com>
2024-12-05 07:50:30 +0000

    Added service layer for api call from dashboard component.

---
+++ dashboard_client/src/app/component/dashboard/dashboard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs'
+
+import {Product} from '../../model/product'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  private apiUrl = 'http://localhost:44327/api/dashboard';
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<Product[]> {
+   return this.http.get<Product[]>(this.apiUrl);
+  }
+}
