8e642183fc365c53215f666469a071662f4653f4
SudhaJarathi <sudha.sai@gmail.com>
2024-12-06 12:28:45 +0000

    Added database scripts to create dashboard database and product table.

---
+++ Database/ProductTable.sql
@@ -0,0 +1,24 @@
+USE [Dashboard]
+GO
+
+/****** Object:  Table [dbo].[Product]    Script Date: 06/12/2024 12:26:43 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Product](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nchar](50) NOT NULL,
+	[Description] [nchar](200) NULL,
+	[Category] [nchar](50) NOT NULL,
+	[Price] [float] NOT NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
